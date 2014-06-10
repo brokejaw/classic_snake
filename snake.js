515bc5f0581cf992f892e5d22c0b89c7b23ffae4
stephen brokaw <brokaw.stephen@gmail.com>
2014-06-09 21:33:43 -0700

    initial commit

---
+++ snake.js
@@ -0,0 +1,9 @@
+// this function waits until the document is ready before
+// it loads. 
+// binding portions (code that modifies the DOM) should be placed
+// in the wait-for-ready function. 
+(function(root) {
+	
+	var Game = root.Game = (root.Game || {});
+	
+})(this);
